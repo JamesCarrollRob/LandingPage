b2d150efa16c8421ddf109d3523cc20e5aa87bb0
JamesCarrollRob <46402913+JamesCarrollRob@users.noreply.github.com>
2022-06-30 17:21:10 -0400

    added some javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+const clickbait = document.getElementById('clickbait');
+
+function clickButton(){
+    alert("this button does nothing significant! Thank you!" );
+}
+
+clickbait.addEventListener("click", clickButton);
